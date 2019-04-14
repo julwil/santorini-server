3dedb5aca5aa7dfc2b449ff290b447a2fbfb0a41
Areg <araare@hotmail.com>
2019-04-14 23:13:30 +0200

    G02-66 Utilities -> Basic authentication class

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/Utilities/Utilities.java
@@ -0,0 +1,27 @@
+package ch.uzh.ifi.seal.soprafs19.Utilities;
+import ch.uzh.ifi.seal.soprafs19.entity.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional
+public class Utilities {
+
+
+    public void copyAttributes(User toUser, User fromUser) {
+
+
+        toUser.setUsername(
+                fromUser.getUsername() != null ?
+                        fromUser.getUsername() : toUser.getUsername()
+        );
+        toUser.setPassword(
+                fromUser.getPassword() != null ?
+                        fromUser.getPassword() :  toUser.getPassword()
+        );
+        toUser.setName(fromUser.getName());
+        toUser.setBirthday(fromUser.getBirthday());
+
+    }
+}
