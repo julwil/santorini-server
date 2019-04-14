6e450da3a18be4c970997197ee2d194ce5b3c6b2
Areg <araare@hotmail.com>
2019-04-14 23:10:49 +0200

    G02-66 Utilities -> Basic authentication class

--- src/main/java/ch/uzh/ifi/seal/soprafs19/repository/GameRepository.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/repository/GameRepository.java
@@ -1,7 +1,6 @@
 package ch.uzh.ifi.seal.soprafs19.repository;
 
 import ch.uzh.ifi.seal.soprafs19.entity.Game;
-import ch.uzh.ifi.seal.soprafs19.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
